acea3161c4204127678d5d27ff38621d7a0eb89b
root <root@DESKTOP-34LQD19>
2025-04-10 15:10:27 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+/*******************************************************
+ *
+ *    _    ___  ___   ___  ___ __   __ ___  ___  ___
+ *   /_\  |_ _||   \ | _ \|_ _|\ \ / /| __|| _ \/ __|
+ *  / _ \  | | | |) ||   / | |  \ V / | _| |   /\__ \
+ * /_/ \_\|___||___/ |_|_\|___|  \_/  |___||_|_\|___/
+ *
+ *
+ * Copyright (C) 2025 AIOS @ AIDRIVERS Ltd - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * author = 'Adwait Naik'
+ * email  = 'adwait@aidrivers.ai'
+ *
+ *******************************************************/
+
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+int main() {
+    //std::cout << "Hello, World!" << std::endl;
+    //std::cout << "PSA APM project restructuring" << std::endl;
+    spdlog::info("Hello, World!");
+    spdlog::info("PSA APM project restructuring");
+    return 0;
+}
