cf18e1f3a5d5734854ca4aa655b2a625dee88096
adwaitnaik97 <adwait@aidrivers.ai>
2025-04-11 11:18:17 +0100

    Revert "Moved submodules to src/components/sensors folder"
    This reverts commit 26ff45e0083c7888a5a815c07e503dcda687390a.

--- src/components/sensors/ehmi_driver/README.md
+++
@@ -1,1 +0,0 @@
-# ehmi-driver
--- src/components/sensors/honeywell/hg_node/src/HGuideAPI/include/utils/Checksum.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-HGUIDE_DLL uint32_t computeChecksum(uint32_t* buffer, unsigned int size);
-HGUIDE_DLL uint16_t computeChecksum16Bit(uint8_t *buffer, unsigned int size);
-HGUIDE_DLL unsigned long Calculate32BitCRC(const unsigned char * data, unsigned long length);
-HGUIDE_DLL unsigned long computeNovatelCRC32(unsigned char *ucBuffer, unsigned long ulCount);
